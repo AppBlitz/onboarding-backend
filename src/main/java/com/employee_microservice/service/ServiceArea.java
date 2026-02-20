bd22e00074aa5933a97fdc4131ad4e738e952684
Carlos Fabian Corrales Zapata <fabianzapata98@outlook.com>
2026-02-20 16:31:16 -0500

    organized error

---
+++ src/main/java/com/employee_microservice/service/ServiceArea.java
@@ -0,0 +1,16 @@
+package com.employee_microservice.service;
+
+import java.util.List;
+
+import com.employee_microservice.model.entitys.Area;
+import com.employee_microservice.service.intefacesService.interfaceArea;
+
+public class ServiceArea implements interfaceArea {
+
+  @Override
+  public List<Area> networkihgAllArea() {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'networkihgAllArea'");
+  }
+
+}
---
+++ src/main/java/com/employee_microservice/service/intefacesService/interfaceArea.java
@@ -0,0 +1,12 @@
+package com.employee_microservice.service.intefacesService;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.employee_microservice.model.entitys.Area;
+
+public interface interfaceArea {
+  public List<Area> networkihgAllArea();
+
+}
